805502dff369353773c150a3a43daf5edfba1b9b
liamsegura <liamdiegosegura@gmail.com>
2022-04-25 01:03:10 +0100

    updates, searchbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink)
+document.querySelector('input').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    getDrink()
+  }
+});
+
+
 
 function getDrink(){
     let drink = document.querySelector('input').value 
@@ -16,3 +23,5 @@ function getDrink(){
                       console.log(`error ${err}`)
                  });
     }
+
+    
