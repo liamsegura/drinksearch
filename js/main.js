2d402caf2b23b7bc235a38e402585f66a8e31e3b
liamsegura <liamdiegosegura@gmail.com>
2022-05-10 10:39:54 +0100

    added A catagory with append child on ul

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.querySelector('input').addEventListener('keypress', function (e) {
     getDrink()
   }
 });
-
+document.getElementById('A').addEventListener('click', searchA)
 
 
 function getDrink(){
@@ -23,5 +23,27 @@ function getDrink(){
                       console.log(`error ${err}`)
                  });
     }
+
+const list = document.querySelector('ul')
+function searchA(){
+          fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
+                .then(res => res.json()) // parse response as JSON
+                .then(data => {
+                  console.log(data.drinks)
+                  for(object of data.drinks){
+                    console.log(object.strDrink)
+                    const item = document.createElement('li')
+                    item.innerText = object.strDrink
+                    list.appendChild(item)
+                    
+                  }
+                  // document.querySelector('h2').innerText = data.drinks[0].strDrink
+                  // document.querySelector('img').src = data.drinks[0].strDrinkThumb
+                  // document.querySelector('h3').innerText = data.drinks[0].strInstructions
+                })
+                .catch(err => {
+                    console.log(`error ${err}`)
+               });
+  }
 
     
