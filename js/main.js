e290352a50ce28e3f8c77b19d1d095aed9b2f3c1
liamsegura <liamdiegosegura@gmail.com>
2022-05-10 15:38:40 +0100

    adding popup

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ document.querySelector('input').addEventListener('keypress', function (e) {
     getDrink()
   }
 });
-document.getElementById('A').addEventListener('click', searchA)
-
+// document.querySelector('.main-container').addEventListener('click', drinkInfo)
 
 function getDrink(){
     let drink = document.querySelector('input').value 
@@ -24,26 +23,38 @@ function getDrink(){
                  });
     }
 
-const list = document.querySelector('ul')
-function searchA(){
-          fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
-                .then(res => res.json()) // parse response as JSON
-                .then(data => {
-                  console.log(data.drinks)
-                  for(object of data.drinks){
-                    console.log(object.strDrink)
-                    const item = document.createElement('li')
-                    item.innerText = object.strDrink
-                    list.appendChild(item)
-                    
-                  }
-                  // document.querySelector('h2').innerText = data.drinks[0].strDrink
-                  // document.querySelector('img').src = data.drinks[0].strDrinkThumb
-                  // document.querySelector('h3').innerText = data.drinks[0].strInstructions
-                })
-                .catch(err => {
-                    console.log(`error ${err}`)
-               });
-  }
+// function drinkInfo(){
+//   let drink = document.querySelector('input').value 
+//   let drinkUnderscore = drink.replace(/ /g,"_")
+//           fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkUnderscore}`)
+//                 .then(res => res.json()) // parse response as JSON
+//                 .then(data => {
+//                   console.log(data.drinks[0])
+//                   document.querySelector('h2').innerText = data.drinks[0].strDrink
+//                   document.querySelector('img').src = data.drinks[0].strDrinkThumb
+//                   document.querySelector('h3').innerText = data.drinks[0].strInstructions
+//                 })
+//                 .catch(err => {
+//                     console.log(`error ${err}`)
+//                });
+// }
 
-    
+const model = document.querySelector('.model')
+const previews = document.querySelectorAll('.main-container')
+const original = document.querySelector('.full-img')
+const imgText = document.querySelector('.caption')
+
+previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+        model.classList.add('open')
+        original.classList.add('open')
+        original.src = 'https://via.placeholder.com/650'
+    })
+})
+
+model.addEventListener('click', (e) => {
+    if(e.target.classList.contains('model')){
+        model.classList.remove('open')
+        original.classList.remove('open')
+    }
+  })
