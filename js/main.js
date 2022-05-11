0fc5e80d85b02365d92073b174cbd5ad851f201c
liamsegura <liamdiegosegura@gmail.com>
2022-05-11 11:57:24 +0100

    adding browse by letter

--- js/main.js
+++ js/main.js
@@ -60,6 +60,7 @@ function getDrink(){
                     // console.log(item)
                     // // //  item.classList.add('card-container')
                     // list.appendChild(item)
+
                        //add show class to containers for search results - display:flex when drink is searched for(originally display:block)
                        previews.forEach(preview => {
                         preview.classList.add('show')
@@ -82,32 +83,58 @@ function getDrink(){
                  });
     }
 
-   //CODE FOR APENDING TO LIST
+   //CODE FOR APENDING TO LIST(ALPHABET)
+ 
+  ['a','b','c','d','e','f','g','h',
+  'i','j','k','l','m','n','o','p','q',
+  'r','s','t','u','v','w','x','y','z'].forEach(choice => document.getElementById(choice).addEventListener('click', () => letterChoice(choice)))
 
+    function letterChoice(choice){
+              fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${choice}`)
+                    .then(res => res.json()) // parse response as JSON
+                    .then(data => {
+                      console.log()
+                      for(object of data.drinks){
+                        console.log(object.strDrink)
+                        const item = document.createElement('li')
+                        const name = document.createElement('h2')
+                        const img = document.createElement('img')
+                        const ingredients = document.createElement('p')
 
-//     function searchA(){
-//               fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
-//                     .then(res => res.json()) // parse response as JSON
-//                     .then(data => {
-//                       console.log(data.drinks)
-//                       for(object of data.drinks){
-//                         // console.log(object.strDrink)
-//                         const item = document.createElement('li')
-//                         item.innerText = object.strDrink
-//                         item.classList.add('card-container')
-//                         list.appendChild(item)
-//                       }
-//                       // document.querySelector('h2').innerText = data.drinks[0].strDrink
-//                       // document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//                       // document.querySelector('h3').innerText = data.drinks[0].strInstructions
-//                     })
-//                     .catch(err => {
-//                         console.log(`error ${err}`)
-//                    });
-//       }
-// searchA()
+                        img.style.margin = "0 auto";
+                        img.style.maxWidth = "140px";
+                        img.style.maxWeight = "140px";
+                        img.style.borderRadius = "50%";
+                        img.src = object.strDrinkThumb
 
+                        item.appendChild(img)
 
+                        name.style.fontSize = "1.1rem";
+                        name.innerText = object.strDrink
+
+                        item.appendChild(name)
+
+                        item.style.alignItems = "center";
+                        item.style.justifyContent = "center";
+                        item.style.cursor = "pointer";
+                        item.style.padding = "10px";
+                        item.style.borderRadius = "10px";
+                        item.style.backgroundColor = "rgb(233, 232, 232)";
+                        // item.innerText = object.strDrink
+
+                       
+                        
+                        // item.classList.add('card-container')
+                        list.appendChild(item)
+                      }
+                      // document.querySelector('h2').innerText = data.drinks[0].strDrink
+                      // document.querySelector('img').src = data.drinks[0].strDrinkThumb
+                      // document.querySelector('h3').innerText = data.drinks[0].strInstructions
+                    })
+                    .catch(err => {
+                        console.log(`error ${err}`)
+                   });
+      }
 //TEST CODE BELOW
 
 
