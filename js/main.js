0a0fc0f11e7378b7772963a7a4e300cfeb9e1f63
liamsegura <liamdiegosegura@gmail.com>
2022-05-12 12:18:00 +0100

    changed nav around, added grid to cards, optimized code and commented, fixed css stylesheet, changed names of variables

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
-//pop up section
-const model = document.querySelector('.model')
-//container with results from search
-const previews = document.querySelectorAll('.card-container')
-//image for popup model
-const original = document.querySelector('.full-img')
-//instructions for popup model 
-const imgText = document.querySelector('.caption')
 //results list
 const list = document.querySelector('.results')
+//container with results from search
+const previews = document.querySelectorAll('.card-container')
+//pop up section
+const model = document.querySelector('.model')
+//bane for popup model
+const modelName = document.querySelector('.model-name')
+//image for popup model
+const modelImage = document.querySelector('.model-img')
+//instructions for popup model 
+const modelInstructions = document.querySelector('.model-instructions')
 
-//event listener for when clicking on container, to activate and add the popup model
-previews.forEach(preview => {
-  preview.addEventListener('click', () => {
-    //adds open class to display as block when clicked
-      model.classList.add('open')
-      original.classList.add('open')
-  })
-})
-//event listener for when clicking off of the pop up model
-model.addEventListener('click', (e) => {
-  //if the element clicked contains model class, remove the open class
-  if(e.target.classList.contains('model')){
-      model.classList.remove('open')
-      original.classList.remove('open')
-  }
-})
+
+
+
 //search bar button
 document.querySelector('button').addEventListener('click', getDrink)
 //search bar enterkey(i know i have done this the long way)
@@ -56,73 +45,66 @@ function getDrink(){
                    });
       }
     }
-   //CODE FOR APENDING TO LIST(ALPHABET)
+   
+   
+//CODE FOR APENDING TO LIST(ALPHABET)
  
-  ['a','b','c','d','e','f','g','h',
-  'i','j','k','l','m','n','o','p','q',
-  'r','s','t','u','v','w','x','y','z'].forEach(choice => document.getElementById(choice).addEventListener('click', () => letterChoice(choice)))
+// event listener for each letter
+['a','b','c','d','e','f','g','h',
+'i','j','k','l','m','n','o','p','q',
+'r','s','t','u','v','w','x','y','z'].forEach(choice => document.getElementById(choice).addEventListener('click', () => letterChoice(choice)))
 
-    function letterChoice(choice){
-      list.innerHTML = ""
-              fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${choice}`)
-                    .then(res => res.json()) // parse response as JSON
-                    .then(data => {
-                      list.innerHtml = ""
-                      data.drinks.forEach(result => {
-                        console.log(result.strDrink)
-                        list.appendChild(createItem(result))
-                      })
-                    }) 
-                    .catch(err => {
-                        console.log(`error ${err}`)
-                   });
-      }
+
+// function for alphabet selection
+function letterChoice(choice){
+  list.innerHTML = ""
+          fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${choice}`)
+                .then(res => res.json()) // parse response as JSON
+                .then(data => {
+                  //clear DOM
+                  list.innerHtml = ""
+                  data.drinks.forEach(result => {
+                    //appends variable createItem with results from data.drinks to list variable
+                    list.appendChild(createItem(result))
+                  })
+                }) 
+                .catch(err => {
+                    console.log(`error ${err}`)
+               });
+  }
+
+//variable to create HTML with a param for data
 const createItem = (result) => {
-        let newLi = document.createElement('li');
-            newLi.className = 'card-container'
-            newLi.innerHTML =  ` <img class="card-img" src="${ result.strDrinkThumb }" alt="${ result.strDrink }"> <div class="name-ingredients">   <h2 class="drink-name">${ result.strDrink }</h2>   <h3 class="drink-instructions">${ result.strInstructions }</h3> </div> `
-          // event listeners for each results
-                        newLi.addEventListener('click', () => {
-                          console.log('test')
-                          //adds open class to display model and image
-                            model.classList.add('open')
-                            original.classList.add('open')
-                            //adds data from api into the DOM
-                            original.src = result.strDrinkThumb
-                            imgText.innerText = result.strDrink
-                        })
-          return newLi
-      }
-  //   preview.addEventListener('click', () => {
-  //     //adds open class to display model and image
-  //       model.classList.add('open')
-  //       original.classList.add('open')
-  //       //adds data from api into the DOM
-  //       original.src = data.drinks[0].strDrinkThumb
-  //       imgText.innerText = data.drinks[0].strInstructions
-  //   })
-  // })
+  //HTML for the DOM with results from the param
+    let newLi = document.createElement('li');
+        newLi.className = 'card-container'
+        newLi.innerHTML =  ` <img class="card-img" src="${ result.strDrinkThumb }" alt="${ result.strDrink }">
+                             <div class="name-ingredients">   
+                                  <span class="drink-name">${ result.strDrink }</span>   
+                                  <p class="drink-instructions">${ result.strInstructions }</p> 
+                             </div> `
 
-//TEST CODE BELOW
-
-
-// document.querySelector('.main-container').addEventListener('click', drinkInfo)
-
-
-// function drinkInfo(){
-//   let drink = document.querySelector('input').value 
-//   let drinkUnderscore = drink.replace(/ /g,"_")
-//           fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkUnderscore}`)
-//                 .then(res => res.json()) // parse response as JSON
-//                 .then(data => {
-//                   console.log(data.drinks[0])
-//                   document.querySelector('h2').innerText = data.drinks[0].strDrink
-//                   document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//                   document.querySelector('h3').innerText = data.drinks[0].strInstructions
-//                 })
-//                 .catch(err => {
-//                     console.log(`error ${err}`)
-//                });
-// }
-
-
+        // event listeners for each results
+        newLi.addEventListener('click', () => {
+        //adds open class to display model and image
+        model.classList.add('open')
+        modelName.classList.add('open')
+        modelImage.classList.add('open')
+        modelInstructions.classList.add('open')
+        //adds data from api into the DOM
+        modelImage.src = result.strDrinkThumb
+        modelInstructions.innerText = result.strInstructions
+        modelName.innerText = result.strDrink
+      })
+        //event listener for when clicking off of the pop up model
+        model.addEventListener('click', (e) => {
+        //if the element clicked contains model class, remove the open class
+        if(e.target.classList.contains('model')){
+            model.classList.remove('open')
+            modelName.classList.remove('open')
+            modelImage.classList.remove('open')
+            modelInstructions.classList.remove('open')
+        }
+      })
+      return newLi
+  }
