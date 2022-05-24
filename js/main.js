0cac62177ba081eae4082ecbda1f185b51e357db
liamsegura <liamdiegosegura@gmail.com>
2022-05-25 00:12:29 +0100

    fixed hero p

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ for(let n of counters) {
   const updateCount = () => {
     const target = + n.getAttribute('data-target');
     const count = + n.innerText;
-    const speed = 5000; // change animation speed here
+    const speed = 2000; // change animation speed here
     const inc = target / speed; 
     if(count < target) {
       n.innerText = Math.ceil(count + inc);
