b451acdd57c5a2017bf4315e5b35659bcb5cb538
liamsegura <liamdiegosegura@gmail.com>
2022-05-24 16:57:21 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 
+// Selector
+const counters = document.querySelectorAll('.counter');
+// Main function
+for(let n of counters) {
+  const updateCount = () => {
+    const target = + n.getAttribute('data-target');
+    const count = + n.innerText;
+    const speed = 5000; // change animation speed here
+    const inc = target / speed; 
+    if(count < target) {
+      n.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      n.innerText = target;
+    }
+  }
+  updateCount();
+}
+
 //results list
 const list = document.querySelector('.results')
 
